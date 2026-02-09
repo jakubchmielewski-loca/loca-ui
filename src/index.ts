67ed8e2f72ae8b562ae8e2e8f4bc657e92232d4c
Jakub Chmielewski <kuba@codeblade.pl>
2026-02-09 13:54:50 +0100

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-export * from "./components/page-pane";
-export * from "./components/app-footer";
-export * from "./components/navbar";
-export * from "./components/search-input";
 export * from "./components/alt-stepper";
-export * from "./components/table-footer";
-
+export * from "./components/alt-table-th";
+export * from "./components/app-footer";
 export * from "./components/loca-ui-provider";
+export * from "./components/navbar";
+export * from "./components/page-pane";
+export * from "./components/page-wrapper";
+export * from "./components/search-input";
+export * from "./components/table-footer";
 
 export { default as useNavbar } from "./hooks/use-navbar";
 
