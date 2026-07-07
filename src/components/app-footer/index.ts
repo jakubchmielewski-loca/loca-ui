64857d5410f9de35e54db151332b9041620f6299
Jakub Chmielewski <kuba@codeblade.pl>
2026-07-07 09:07:34 +0200

    fix: bad exports

--- src/components/app-footer/index.ts
+++ src/components/app-footer/index.ts
@@ -1,11 +1,4 @@
-export {
-  AppFooter,
-  type AppFooterOptions,
-  type AppFooterProps,
-  type AppFooterServiceLink,
-  type AppFooterServicesConfig,
-  type AppFooterUser,
-} from "./app-footer";
+export { AppFooter } from "./app-footer";
 
 export { AppFooterProvider } from "./provider";
 export { AppFooterContext } from "./context";
